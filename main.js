3f7c9eb7a6013d70b71c3c4be3508a8dbc493527
amitkumarjhookiya@gmail.com <amitkumarjhookiya@gmail.com>
2024-01-27 00:12:27 +0530

    3 line abslut position

--- main.js
+++ main.js
@@ -8,61 +8,91 @@ var nav = document.querySelector("nav");
 
 var on = false;
 line_box.addEventListener('click', function() {
-    if (on == false) {
-        yo_nav_slider.style.display = 'block';
-        gsap.to('nav', {
-            background: '#FFFCE1',
-            color: '#0E100F',
-        })
-        gsap.to('.slider_1', {
-            transform: 'translateX(0vw)',
-            rotate: 0,
-            animationTimingFunction: 'cubic - bezier(0.19, 1, 0.22, 1)',
+  if (on == false) {
+    yo_nav_slider.style.display = 'block';
+    gsap.to('nav', {
+      background: '#FFFCE1',
+      color: '#0E100F',
+    })
+    gsap.to('.slider_1', {
+      transform: 'translateX(0vw)',
+      rotate: 0,
+      animationTimingFunction: 'cubic - bezier(0.19, 1, 0.22, 1)',
 
-        })
-        gsap.to('.slider_2', {
-            transform: 'translateX(0vw)',
-            //rotate: 45,
-            animationTimingFunction: 'cubic - bezier(0.19, 1, 0.22, 1)',
+    })
+    gsap.to('.slider_2', {
+      transform: 'translateX(0vw)',
+      //rotate: 45,
+      animationTimingFunction: 'cubic - bezier(0.19, 1, 0.22, 1)',
 
-        })
-        gsap.to('.right_nav_monu', {
-            border: '0.2vw solid #0E100F',
-        })
-        gsap.to('.line', {
-            background: '#0E100F',
+    })
+    gsap.to('.right_nav_monu', {
+      border: '0.2vw solid #0E100F',
+    })
+    gsap.to('.line', {
+      background: '#0E100F',
+    })
+    on = true;
 
-        })
-        on = true;
-    }
-    else {
+    var yo_1 = gsap.timeline();
+    yo_1
 
-        gsap.to('nav', {
-            background: '#0E100F',
-            color: '#FFFCE1',
-        })
-        gsap.to('.slider_1', {
-            transform: 'translateX(140vw)',
-            //rotate: 45,
-            animationTimingFunction: 'cubic - bezier(0.19, 1, 0.22, 1)',
+      .to('.line_1', {
+        rotate: 45,
+      })
+      .to('.line_3', {
+        rotate: -45,
+      })
 
-        })
-        gsap.to('.slider_2', {
-            transform: 'translateX(-140vw)',
-            //rotate: 45,
-            animationTimingFunction: 'cubic - bezier(0.19, 1, 0.22, 1)',
+      .to('.line_2', {
+        display:'none'
+      })
 
-        })
-        gsap.to('.right_nav_monu', {
-            border: '0.2vw solid #FFFCE1',
-        })
-        gsap.to('.line', {
-            background: '#FFFCE1',
 
-        })
-        on = false;
+      .from(".line", {
+        width: 0,
+        y: 0,
 
-    }
+      })
+
+
+
+
+
+    // -----+
+
+
+
+
+  }
+  else {
+
+    gsap.to('nav', {
+      background: '#0E100F',
+      color: '#FFFCE1',
+    })
+    gsap.to('.slider_1', {
+      transform: 'translateX(140vw)',
+      //rotate: 45,
+      animationTimingFunction: 'cubic - bezier(0.19, 1, 0.22, 1)',
+
+    })
+    gsap.to('.slider_2', {
+      transform: 'translateX(-140vw)',
+      //rotate: 45,
+      animationTimingFunction: 'cubic - bezier(0.19, 1, 0.22, 1)',
+
+    })
+    gsap.to('.right_nav_monu', {
+      border: '0.2vw solid #FFFCE1',
+    })
+    gsap.to('.line', {
+      background: '#FFFCE1',
+
+    })
+    on = false;
+
+  }
 
 
 
@@ -71,37 +101,12 @@ line_box.addEventListener('click', function() {
 })
 //yo_nav_slider_on_btn.style.animationDuration
 
-var timeline = gsap.timeline({
-    duration:0.5,
-});
-console.log(timeline)
-
-// Add animations to the timeline
-timeline.to(".line", {
-    width: 0,
-}).to(".line_2", {
-    display: 'none'
-}).to(".line", {
-    width: "6vw",
-}).to('.line_1', {
-    rotate: 45,
-    position: 'absolute'
-
-}).to('.line_3', {
-    rotate: -45,
-    position: 'absolute'
-
-
-})
-
-
-
 
 
 
 
 gsap.from('.yo_hero_text_1 h1,.yo_hero_text_2 h1', {
-    // rotate: 40,
-    letterSpacing: "-2.7vw",
-    duration: 2,
+  // rotate: 40,
+  letterSpacing: "-2.7vw",
+  duration: 2,
 })
